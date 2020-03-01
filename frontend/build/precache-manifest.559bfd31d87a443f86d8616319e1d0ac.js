95bbca4505b14f07af1bd34705d13650f58536f2
Ahmed Gamal <ahmedmgh67@gmail.com>
2020-03-01 20:23:21 +0200

    build

---
+++ frontend/build/precache-manifest.559bfd31d87a443f86d8616319e1d0ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "685c6e5ad02bc90f087e39f3ddda4b57",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5fd39dea6e3f87ddaae",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "b5fd39dea6e3f87ddaae",
+    "url": "/static/js/2.eb08f306.chunk.js"
+  },
+  {
+    "revision": "f4c221c777cee4a68350",
+    "url": "/static/js/main.10bdc81b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "79b4d48d465a4ceb49e6747eb33a4404",
+    "url": "/static/media/background.79b4d48d.jpg"
+  },
+  {
+    "revision": "d60d852e9643287d4065be5e71c70b62",
+    "url": "/static/media/background.d60d852e.jpeg"
+  }
+]);
